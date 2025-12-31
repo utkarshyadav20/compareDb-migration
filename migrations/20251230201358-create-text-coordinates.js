563a1c1166004a9aecaa8bd3de6de950d0509538
RunawayGOD <20022002utkarsh@gmail.com>
2026-01-01 04:35:26 +0530

    added model result table and coordinates column in results table

---
+++ migrations/20251230201358-create-text-coordinates.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('ModelResult', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      Project_id: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      image_name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      Build_id: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      Project_type: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      coords_vs_text: {
+        type: Sequelize.JSONB,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('ModelResult');
+  }
+};
